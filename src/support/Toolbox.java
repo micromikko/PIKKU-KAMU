91bf93a8395eba86cd78f41842e2dedf6630bc40
micromikko <micromikko@gmail.com>
2016-05-02 14:19:18 +0300

    added new method

--- src/support/Toolbox.java
+++ src/support/Toolbox.java
@@ -11,7 +11,8 @@ public class Toolbox {
 
 	public static final double PI = 3.14159;
 	public static final double RADIAN = 0.01745;
-	public static final double WHEEL_RADIUS = 1.8;
+	public static final double WHEEL_RADIUS = 18;
+	public static final double BOT_RADIUS = 110;
 
 	public static int driveActual;
 	public static int turnActual;
@@ -26,6 +27,11 @@ public class Toolbox {
 		double theta = distance / WHEEL_RADIUS;
 		int degrees = (int) (theta / RADIAN);
 		return degrees;
+	}
+	
+	public static int botDegToDistance(int botDegrees) {
+		double distance = 2 * PI * BOT_RADIUS * (botDegrees / 360);
+		return (int) distance;
 	}
 }
 
