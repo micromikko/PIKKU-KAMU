5e1c7ab9ed13fd9ab84d840b226f436f151f9ea9
micromikko <micromikko@gmail.com>
2016-05-04 12:35:21 +0300

    asd pitäs olla nyt niinku toimiva

--- src/modes/ManualControl.java
+++ src/modes/ManualControl.java
@@ -1,51 +1,52 @@
 package modes;
 
+import lejos.hardware.sensor.EV3IRSensor;
 import logic.Motors;
 
 public class ManualControl {
 	
 	private Motors motors;
+	private EV3IRSensor irSensor;
 	
-	public ManualControl(Motors importedMotors) {
+	public ManualControl(Motors importedMotors, EV3IRSensor irSensor) {
 		this.motors = importedMotors;
+		this.irSensor = irSensor;
 	}
 	
-	public void manualControl(int remoteCommand, int remoteChannel) {
+	public void manualControl() {
+		
+		int remoteCommand = irSensor.getRemoteCommand(0);
 		
 		switch(remoteCommand) {
 			case 1:
 				// both forward
-				this.motors.setAcceleration(10000000);
-				while (remoteChannel != 0) {
+				this.motors.setDriveAcceleration(10000000);
+				while (irSensor.getRemoteCommand(0) != 0) {
 					this.motors.driveForward();
-		//			UI.bothForward();
 				}
 				this.motors.stopMotors();
 				break;
 			case 2:
 				// left, forward
-				this.motors.setAcceleration(10000000);
-				while (remoteChannel != 0) {
+				this.motors.setDriveAcceleration(10000000);
+				while (irSensor.getRemoteCommand(0) != 0) {
 					this.motors.turnRight();
-		//			UI.leftForward();
 				}
 				this.motors.stopMotors();
 				break;
 			case 3:
 				// both backward
-				this.motors.setAcceleration(10000000);
-				while(remoteChannel != 0) {		// Both motors backward while button is pressed
+				this.motors.setDriveAcceleration(10000000);
+				while(irSensor.getRemoteCommand(0) != 0) {		// Both motors backward while button is pressed
 					this.motors.driveBackward();
-		//			UI.bothBackward();							// prints issued command to screen
 				}
 				this.motors.stopMotors();							// stop all motors. same principle applies to all four controller buttons
 				break;
 			case 4:
 				// right, forward
-				this.motors.setAcceleration(10000000);
-				while (remoteChannel != 0) {
+				this.motors.setDriveAcceleration(10000000);
+				while (irSensor.getRemoteCommand(0) != 0) {
 					this.motors.turnLeft();
-		//			UI.rightForward();
 				}
 				this.motors.stopMotors();
 				break;
