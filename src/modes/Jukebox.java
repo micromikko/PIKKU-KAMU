5f43321b22c387be13c63303b4ffb19ef1fdb595
joelko <joelko@metropolia.fi>
2016-05-09 16:02:12 +0300

    Update Jukebox.java
    jaskkakakakaa

--- src/modes/Jukebox.java
+++ src/modes/Jukebox.java
@@ -1,12 +1,11 @@
-package modes;
-
-//package music;
+package music;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import lejos.hardware.Button;
+import lejos.hardware.Audio;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3IRSensor;
@@ -16,7 +15,7 @@ public class Jukebox extends Thread {
 	final File soundFile1 = new File("Space_Alert3.wav");
 	final File soundFile2 = new File("laaseri.wav");
 	final File soundFile3 = new File("Space_Alert2.wav");
-	final File soundFile4 = new File("Space_Alert1");
+	final File soundFile4 = new File("Space_Alert1.wav");
 	private EV3IRSensor irSensor;
 	private int soundNumber;
 	private int volume;
@@ -26,49 +25,34 @@ public class Jukebox extends Thread {
 	
 	public Jukebox () {
 		soundNumber = 0;
-		volume = 0;
+		volume = 50;
 		playSound = true;
 		map.put(soundFile1, "Space Alert 3");
 		map.put(soundFile2, "Laaseri");
 		map.put(soundFile3, "Space Alert 2");
 		map.put(soundFile4, "Space Alert 1");
-		playList.add(soundFile1);
-		playList.add(soundFile2);
-		playList.add(soundFile3);
-		playList.add(soundFile4);
-		
+	}
+	
+	public void jaska() {
+		/*for (File J : playList){
+			LCD.clear();
+			LCD.drawString(map.get(J), 0, 0);
+			Sound.playSample(J, volume);
+			Delay.msDelay(5000);
+		}*/
+	}
+	
+	public void musicPlay() {
 		
 	}
 	
-	public void run() {
-		for (File J : playList){
-			if(playSound==true){
-				Sound.playSample(J, volume);
-				LCD.drawString(map.get(J), 0, 0);
-				Delay.msDelay(1000);
-			}	
-		}
-		
-		
-	}
-	public void musicControl() {
-		int remoteCommand = irSensor.getRemoteCommand(0);
-		switch(remoteCommand) {
-			case 1:
-				setVolume(5);
-			case 2:
-				setVolume(-5);
-			case 3:
-				
-			case 4:
-	
-	
-		}
-	}
-	
-	public int setSound(int sound){
+	/*public int setSound(int sound){
 		soundNumber = sound;
 		return sound;
+	}*/
+	
+	public int getVolume(){
+		return volume;
 	}
 	
 	public int setVolume(int a){
@@ -81,17 +65,37 @@ public class Jukebox extends Thread {
 	}
 	
 	public void addToPlayList(){
-		
+		playList.add(soundFile1);
+		playList.add(soundFile2);
+		playList.add(soundFile3);
+		playList.add(soundFile4);
 	}
 	
-	public void removeFromPlayList(int i){
+	/*public void removeFromPlayList(){
+		int remoteCommand = irSensor.getRemoteCommand(0);
 		int a = 0;
+		int i = 0;
+	    while(remoteCommand !=3){
+	    	if(remoteCommand == 1){
+	    		a++;
+	    	}
+	    	Delay.msDelay(500);
+	    }
 		for (File J : playList){
 			i++;
 			if(i==a){
-				playList.add();
-				Delay.msDelay(1000);
-			}	
+				playList.remove(J);
+			}
 		}
+	}*/
+	
+	public void searchPlayList(){
+		LCD.clear();
+		int i = 0;
+		for(File J : playList){
+			LCD.drawString(map.get(J) + i, 0, i);
+			i++;
+		}
+		
 	}
 }
