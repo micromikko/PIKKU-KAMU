94fdd3b6b300e0e5140cd478e95ca6226fb463f6
joelko <joelko@metropolia.fi>
2016-05-09 16:06:28 +0300

    Create MusicController.java

---
+++ src/logic/MusicController.java
@@ -0,0 +1,101 @@
+package music;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import lejos.hardware.Button;
+import lejos.hardware.Audio;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.sensor.EV3IRSensor;
+import lejos.utility.Delay;
+
+public class MusicController extends Thread {
+	final File soundFile1 = new File("Space_Alert3.wav");
+	final File soundFile2 = new File("laaseri.wav");
+	final File soundFile3 = new File("Space_Alert2.wav");
+	final File soundFile4 = new File("Space_Alert1.wav");
+	private EV3IRSensor irSensor;
+	private int soundNumber;
+	private int volume;
+	private boolean playSound;
+	private ArrayList<File> playList = new ArrayList<>();
+	Map<File, String> map = new HashMap<File, String>();
+	
+	public MusicController () {
+		soundNumber = 0;
+		volume = 50;
+		playSound = true;
+		map.put(soundFile1, "Space Alert 3");
+		map.put(soundFile2, "Laaseri");
+		map.put(soundFile3, "Space Alert 2");
+		map.put(soundFile4, "Space Alert 1");
+	}
+	
+	public void jaska() {
+		/*for (File J : playList){
+			LCD.clear();
+			LCD.drawString(map.get(J), 0, 0);
+			Sound.playSample(J, volume);
+			Delay.msDelay(5000);
+		}*/
+	}
+	
+	public void musicPlay() {
+		
+	}
+	
+	/*public int setSound(int sound){
+		soundNumber = sound;
+		return sound;
+	}*/
+	
+	public int getVolume(){
+		return volume;
+	}
+	
+	public int setVolume(int a){
+		this.volume = volume + a;
+		return volume;
+	}
+	
+	public void killSound(){
+		playSound = false;
+	}
+	
+	public void addToPlayList(){
+		playList.add(soundFile1);
+		playList.add(soundFile2);
+		playList.add(soundFile3);
+		playList.add(soundFile4);
+	}
+	
+	/*public void removeFromPlayList(){
+		int remoteCommand = irSensor.getRemoteCommand(0);
+		int a = 0;
+		int i = 0;
+	    while(remoteCommand !=3){
+	    	if(remoteCommand == 1){
+	    		a++;
+	    	}
+	    	Delay.msDelay(500);
+	    }
+		for (File J : playList){
+			i++;
+			if(i==a){
+				playList.remove(J);
+			}
+		}
+	}*/
+	
+	public void searchPlayList(){
+		LCD.clear();
+		int i = 0;
+		for(File J : playList){
+			LCD.drawString(map.get(J) + i, 0, i);
+			i++;
+		}
+		
+	}
+}
