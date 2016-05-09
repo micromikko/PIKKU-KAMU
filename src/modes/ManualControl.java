3c4c3dd61431e6b1451b76a34e90ccc036b01989
micromikko <micromikko@gmail.com>
2016-05-09 15:58:25 +0300

    Add files via upload

--- src/modes/ManualControl.java
+++ src/modes/ManualControl.java
@@ -1,57 +1,70 @@
 package modes;
 
-import lejos.hardware.sensor.EV3IRSensor;
+import logic.IRSListener;
 import logic.Motors;
 
 public class ManualControl {
 	
 	private Motors motors;
-	private EV3IRSensor irSensor;
+	private IRSListener irsl;
+	private boolean armDown = false;
 	
-	public ManualControl(Motors importedMotors, EV3IRSensor irSensor) {
+	public ManualControl(IRSListener irsl, Motors importedMotors) {
 		this.motors = importedMotors;
-		this.irSensor = irSensor;
+		this.irsl = irsl;
 	}
 	
 	public void manualControl() {
 		
-		int remoteCommand = irSensor.getRemoteCommand(0);
+		int remoteCommand = this.irsl.checkForTwoButtonCommands(0);
 		
 		switch(remoteCommand) {
 			case 1:
 				// both forward
 				this.motors.setDriveAcceleration(10000000);
-				while (irSensor.getRemoteCommand(0) != 0) {
+				while (this.irsl.checkForTwoButtonCommands(0) != 0) {
 					this.motors.driveForward();
 				}
-				this.motors.stopMotors();
+				this.motors.stopDriveMotors();
 				break;
 			case 2:
 				// left, forward
 				this.motors.setDriveAcceleration(10000000);
-				while (irSensor.getRemoteCommand(0) != 0) {
+				while (this.irsl.checkForTwoButtonCommands(0) != 0) {
 					this.motors.turnRight();
 				}
-				this.motors.stopMotors();
+				this.motors.stopDriveMotors();
 				break;
 			case 3:
 				// both backward
 				this.motors.setDriveAcceleration(10000000);
-				while(irSensor.getRemoteCommand(0) != 0) {		// Both motors backward while button is pressed
+				while(this.irsl.checkForTwoButtonCommands(0) != 0) {		// Both motors backward while button is pressed
 					this.motors.driveBackward();
 				}
-				this.motors.stopMotors();							// stop all motors. same principle applies to all four controller buttons
+				this.motors.stopDriveMotors();							// stop all motors. same principle applies to all four controller buttons
 				break;
 			case 4:
 				// right, forward
 				this.motors.setDriveAcceleration(10000000);
-				while (irSensor.getRemoteCommand(0) != 0) {
+				while (this.irsl.checkForTwoButtonCommands(0) != 0) {
 					this.motors.turnLeft();
 				}
-				this.motors.stopMotors();
+				this.motors.stopDriveMotors();
 				break;
+			case 8:
+				// arm down
+				armToggle();
 			default:											// if no button is pressed the loop will start again
 		}
 	}
 	
+	public void armToggle() {
+		if(!this.armDown) {
+			this.motors.colorSensorArmDown();
+			this.armDown = true;
+		} else {
+			this.motors.colorSensorArmUp();
+			this.armDown = false;
+		}
+	}
 }
