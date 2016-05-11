012fa98f76ca8d1add2d9c5461828798275d5eec
joelko <joelko@metropolia.fi>
2016-05-11 10:42:48 +0300

    uusi jukebox, pitää testata

--- src/modes/Jukebox.java
+++ src/modes/Jukebox.java
@@ -4,17 +4,11 @@ import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.utility.Delay;
-import logic.MusicController;
 
 public class Jukebox {
 	private EV3IRSensor irSensor;
 	private MusicController jukebox;
 	
-	/**
-	 * Constructor
-	 * @param importedJukebox
-	 * @param irSensor
-	 */
 	public Jukebox(MusicController importedJukebox, EV3IRSensor irSensor){
 		this.jukebox = importedJukebox;
 		this.irSensor = irSensor;
@@ -23,34 +17,60 @@ public class Jukebox {
 		
 	public void musicControl(){
 		while(!Button.ESCAPE.isDown()){
-			LCD.clear();
+			int i = 0;
 			int remoteCommand = irSensor.getRemoteCommand(3);
 			switch(remoteCommand) {
 				case 1:
+					LCD.clear();
 					jukebox.setVolume(5);
 					LCD.drawInt(jukebox.getVolume(), 0, 0);
 					break;
 				case 2:
+					LCD.clear();
 					jukebox.setVolume(-5);
 					LCD.drawInt(jukebox.getVolume(), 0, 0);
 					break;
 				case 3:
+					LCD.clear();
 					jukebox.searchPlayList();
-					LCD.drawInt(3, 0, 0);
 					break;
-			/*case 4:
-				jukebox.removeFromPlayList();*/
 				case 4:
+					while(true)
+					{
+						remoteCommand = irSensor.getRemoteCommand(3);
+						if(remoteCommand==1){
+							LCD.clear();
+							i++;
+							LCD.drawInt(i, 0, 0);
+						}
+						else if(remoteCommand==2){
+							LCD.clear();
+							i--;
+							LCD.drawInt(i, 0, 0);
+						}
+						else if(remoteCommand==3){
+							break;
+						}
+						Delay.msDelay(500);
+						
+					}
+					jukebox.removeFromPlayList(i);
+					break;
+				case 5:
+					LCD.clear();
 					jukebox.jaska();
 					//LCD.drawInt(4, 0, 0);
 					break;
-				case 5:
+				case 8:
+					LCD.clear();
 					jukebox.addToPlayList();
 				default:
 					continue;
 			}
-			Delay.msDelay(500);
+			Delay.msDelay(1000);
 		
 		}
+		LCD.drawString("Exiting Jukebox", 0, 0);
+		Delay.msDelay(1000);
 	}
 }
