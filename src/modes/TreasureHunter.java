034cc5858b6149be45406deb182f8d6ab41cd780
fetsi <tuomas.hyttinen@gmail.com>
2016-05-11 10:42:17 +0300

    Add files via upload

--- src/modes/TreasureHunter.java
+++ src/modes/TreasureHunter.java
@@ -1,14 +1,20 @@
 package modes;
 
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.*;
+import lejos.utility.Delay;
 import logic.ColorSensorArm;
+import logic.ColorChecker;
 import logic.IRSListener;
 import logic.Motors;
+import support.Toolbox;
 
 public class TreasureHunter {
 	
 	private IRSListener irsl;
 	private Motors motors;
 	private ColorSensorArm csa;
+	private ColorChecker colorC = new ColorChecker(SensorPort.S4);
 	
 	private int searchAlgorithm;
 	
@@ -72,6 +78,62 @@ public class TreasureHunter {
 	
 	public void spiralPattern() {
 		
+	}
+	
+	public void zigZagPattern() {
+		csa.setColorSensorArmSpeed(50); 	//limit arm speed to prevent trouble
+		csa.colorSensorArmDown();			//lower arm to reading position
+		LCD.drawString("" + colorC.getCurrentColor(), 1, 1);
+		Delay.msDelay(3000);
+		LCD.clear();
+		
+		motors.setDriveAcceleration(50);
+		
+		
+		int collideCounter = 0; 			//keep track of collisions to search area borders
+		boolean foundTreasure = false;
+		
+		motors.driveForward();
+		
+		while(!foundTreasure) {				//search until treasure found
+			
+			if(colorC.getCurrentColor() == 7) { 	//if we are on a black area
+				//stop, we hit a border of the search area!
+				motors.stopDriveMotors();
+				LCD.drawString("BOINK",1,1);
+				Delay.msDelay(2000);
+				
+				if(collideCounter % 2 == 0) {
+					//check if we have to make a right turn...
+					motors.turnRight(90);
+					Delay.msDelay(500);
+					motors.driveForward(Toolbox.BOT_WIDTH/2); // move the bot laterally by half of its width
+					Delay.msDelay(500);
+					motors.turnRight(90);
+				} else {
+					//...or a left turn
+					motors.turnLeft(90);
+					Delay.msDelay(500);
+					motors.driveForward(Toolbox.BOT_WIDTH/2);
+					Delay.msDelay(500);
+					motors.turnLeft(90);
+				}
+				collideCounter++;							
+				Delay.msDelay(500);
+				motors.driveForward(); //we are facing the right way, let's continue the search by
+						       		   //driving forward
+			}
+			
+			if (colorC.getCurrentColor() == 0) {
+				//we are on a red area, which means we found the treasure!
+				motors.stopDriveMotors();
+				LCD.drawString("Found the treasure!", 1, 1); //declare our findings to the world
+				
+				Delay.msDelay(3000);
+				foundTreasure = true;
+				
+			}
+		}
 	}
 }
 
