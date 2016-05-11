3aaf5b9302592fadfb0b625cc71afcc0fee9731d
micromikko <micromikko@gmail.com>
2016-05-11 16:00:37 +0300

    FINAL

--- src/test/ArmTest.java
+++ src/test/ArmTest.java
@@ -1,94 +1,94 @@
-package test;
-
-import lejos.hardware.sensor.*;
-import lejos.utility.Delay;
-import lejos.hardware.port.SensorPort;
-
-import java.util.List;
-
-import lejos.hardware.Button;
-import lejos.hardware.lcd.*;
-import logic.Motors;
-import logic.IRSListener;
-import logic.ColorChecker;
-import logic.ColorSensorArm;
-import test.ZigZagSearchTest;
-
-public class ArmTest {
-	
-	public static void main(String[] args) {
-		
-		Motors motors = new Motors();
-		ColorSensorArm cs = new ColorSensorArm();
-		ColorChecker colorChecker = new ColorChecker(SensorPort.S4);
-		cs.setColorSensorArmSpeed(100);
-		
-		
-		
-		//List lista = colorChecker.mySensor.getAvailableModes();
-		
-		/*int count = 1;
-		for(Object jee : lista) {
-			LCD.drawString(jee.toString(), 1, count);
-			count++;
-		}
-		
-		while(!Button.ESCAPE.isDown()) {
-			continue;
-		}*/
-		
-		Object paska = colorChecker.mySensor.getColorIDMode();
-		LCD.drawString(paska.toString(), 1, 1);
-		
-		Delay.msDelay(5000);
-		
-		
-		
-	
-		
-		//ZigZagSearchTest searchtest = new ZigZagSearchTest();
-		
-		//LCD.drawString("Preparing for treasure hunt", 1, 1);
-		//Delay.msDelay(3000);
-		//LCD.clear();
-		
-		//searchtest.doSearch();
-		
-		/*while(!Button.ESCAPE.isDown()) {
-			LCD.clear();
-			LCD.drawString("" + colorChecker.getCurrentColor(), 1, 1);
-			Delay.msDelay(100);
-			
-		}*/
-		
-		
-		
-		/* motors.colorSensorArmDown();
-		LCD.drawString("" + motors.getColorSensorArmPosition(), 1, 1);
-		Delay.msDelay(3000);
-		LCD.clear();
-		motors.colorSensorArmUp();
-		LCD.drawString("" + motors.getColorSensorArmPosition(), 1, 1);
-		Delay.msDelay(3000);
-		LCD.clear();
-		motors.colorSensorArmDown();
-		
-		Delay.msDelay(3000);
-		colorChecker.start();
-		LCD.clear();
-		
-		while(!Button.ESCAPE.isDown()) {
-			LCD.clear();
-			LCD.drawString("" + colorChecker.getCurrentColor(), 1, 1);
-			Delay.msDelay(300); 
-			
-		}
-		*/
-
-		
-	
-	}
-	
-	
-
-}
+//package test;
+//
+//import lejos.hardware.sensor.*;
+//import lejos.utility.Delay;
+//import lejos.hardware.port.SensorPort;
+//
+//import java.util.List;
+//
+//import lejos.hardware.Button;
+//import lejos.hardware.lcd.*;
+//import logic.Motors;
+//import logic.IRSListener;
+//import logic.ColorChecker;
+//import logic.ColorSensorArm;
+//import test.ZigZagSearchTest;
+//
+//public class ArmTest {
+//	
+//	public static void main(String[] args) {
+//		
+//		Motors motors = new Motors();
+//		ColorSensorArm cs = new ColorSensorArm();
+//		ColorChecker colorChecker = new ColorChecker(SensorPort.S4);
+//		cs.setColorSensorArmSpeed(100);
+//		
+//		
+//		
+//		//List lista = colorChecker.mySensor.getAvailableModes();
+//		
+//		/*int count = 1;
+//		for(Object jee : lista) {
+//			LCD.drawString(jee.toString(), 1, count);
+//			count++;
+//		}
+//		
+//		while(!Button.ESCAPE.isDown()) {
+//			continue;
+//		}*/
+//		
+//		Object paska = colorChecker.mySensor.getColorIDMode();
+//		LCD.drawString(paska.toString(), 1, 1);
+//		
+//		Delay.msDelay(5000);
+//		
+//		
+//		
+//	
+//		
+//		//ZigZagSearchTest searchtest = new ZigZagSearchTest();
+//		
+//		//LCD.drawString("Preparing for treasure hunt", 1, 1);
+//		//Delay.msDelay(3000);
+//		//LCD.clear();
+//		
+//		//searchtest.doSearch();
+//		
+//		/*while(!Button.ESCAPE.isDown()) {
+//			LCD.clear();
+//			LCD.drawString("" + colorChecker.getCurrentColor(), 1, 1);
+//			Delay.msDelay(100);
+//			
+//		}*/
+//		
+//		
+//		
+//		/* motors.colorSensorArmDown();
+//		LCD.drawString("" + motors.getColorSensorArmPosition(), 1, 1);
+//		Delay.msDelay(3000);
+//		LCD.clear();
+//		motors.colorSensorArmUp();
+//		LCD.drawString("" + motors.getColorSensorArmPosition(), 1, 1);
+//		Delay.msDelay(3000);
+//		LCD.clear();
+//		motors.colorSensorArmDown();
+//		
+//		Delay.msDelay(3000);
+//		colorChecker.start();
+//		LCD.clear();
+//		
+//		while(!Button.ESCAPE.isDown()) {
+//			LCD.clear();
+//			LCD.drawString("" + colorChecker.getCurrentColor(), 1, 1);
+//			Delay.msDelay(300); 
+//			
+//		}
+//		*/
+//
+//		
+//	
+//	}
+//	
+//	
+//
+//}
--- src/test/ZigZagSearchTest.java
+++ src/test/ZigZagSearchTest.java
@@ -1,82 +1,82 @@
-package test;
-
-import lejos.hardware.sensor.*;
-import lejos.utility.Delay;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.lcd.*;
-import logic.Motors;
-import logic.IRSListener;
-import logic.ColorChecker;
-import support.Toolbox;
-
-
-public class ZigZagSearchTest {
-	
-	Motors motors;
-	EV3IRSensor irsensor;
-	IRSListener listener;
-	ColorChecker colorChecker;
-	
-	
-	public ZigZagSearchTest() {
-		//this.irsensor = new EV3IRSensor(SensorPort.S1);
-		this.motors = new Motors();
-		//this.listener = new IRSListener(this.motors, this.irsensor);
-		this.colorChecker = new ColorChecker(SensorPort.S4);
-		
-	}
-	
-	public void doSearch() {
-		motors.setColorSensorArmSpeed(50);
-		motors.colorSensorArmDown();
-		LCD.drawString("" + colorChecker.getCurrentColor(), 1, 1);
-		Delay.msDelay(3000);
-		LCD.clear();
-		
-		motors.motorSync(motors.getMotorLeft(), motors.getMotorRight());
-		motors.setDriveAcceleration(50);
-		
-		
-		int collideCounter = 0;
-		boolean foundTreasure = false;
-		
-		motors.driveForward();
-		
-		while(!foundTreasure) {
-			
-			if(colorChecker.getCurrentColor() == 7) {
-				//stop, we hit a border of the search area!
-				motors.stopDriveMotors();
-				LCD.drawString("BOINK",1,1);
-				Delay.msDelay(2000);
-				
-				if(collideCounter % 2 == 0) {
-					//check if we have to make a left or right turn
-					motors.turnRight(90);
-					Delay.msDelay(500);
-					motors.driveForward(Toolbox.BOT_WIDTH/2); //FIXME: adjust this distance according to the width of the device
-					Delay.msDelay(500);
-					motors.turnRight(90);
-				} else {
-					motors.turnLeft(90);
-					Delay.msDelay(500);
-					motors.driveForward(Toolbox.BOT_WIDTH/2); //FIXME: adjust this distance according to the width of the device
-					Delay.msDelay(500);
-					motors.turnLeft(90);
-				}
-				collideCounter++;
-				Delay.msDelay(500);
-				motors.driveForward(); //we are facing the right way, let's continue the search by
-						       		   //driving forward
-			}
-			
-			if (colorChecker.getCurrentColor() == 0) {
-				motors.stopDriveMotors();
-				LCD.drawString("Found the treasure!", 1, 1);
-				Delay.msDelay(3000);
-				foundTreasure = true;
-				
-			}
-		}
-	}
-}
+//package test;
+//
+//import lejos.hardware.sensor.*;
+//import lejos.utility.Delay;
+//import lejos.hardware.port.SensorPort;
+//import lejos.hardware.lcd.*;
+//import logic.Motors;
+//import logic.IRSListener;
+//import logic.ColorChecker;
+//import support.Toolbox;
+//
+//
+//public class ZigZagSearchTest {
+//	
+//	Motors motors;
+//	EV3IRSensor irsensor;
+//	IRSListener listener;
+//	ColorChecker colorChecker;
+//	
+//	
+//	public ZigZagSearchTest() {
+//		//this.irsensor = new EV3IRSensor(SensorPort.S1);
+//		this.motors = new Motors();
+//		//this.listener = new IRSListener(this.motors, this.irsensor);
+//		this.colorChecker = new ColorChecker(SensorPort.S4);
+//		
+//	}
+//	
+//	public void doSearch() {
+//		motors.setColorSensorArmSpeed(50);
+//		motors.colorSensorArmDown();
+//		LCD.drawString("" + colorChecker.getCurrentColor(), 1, 1);
+//		Delay.msDelay(3000);
+//		LCD.clear();
+//		
+//		motors.motorSync(motors.getMotorLeft(), motors.getMotorRight());
+//		motors.setDriveAcceleration(50);
+//		
+//		
+//		int collideCounter = 0;
+//		boolean foundTreasure = false;
+//		
+//		motors.driveForward();
+//		
+//		while(!foundTreasure) {
+//			
+//			if(colorChecker.getCurrentColor() == 7) {
+//				//stop, we hit a border of the search area!
+//				motors.stopDriveMotors();
+//				LCD.drawString("BOINK",1,1);
+//				Delay.msDelay(2000);
+//				
+//				if(collideCounter % 2 == 0) {
+//					//check if we have to make a left or right turn
+//					motors.turnRight(90);
+//					Delay.msDelay(500);
+//					motors.driveForward(Toolbox.BOT_WIDTH/2); //FIXME: adjust this distance according to the width of the device
+//					Delay.msDelay(500);
+//					motors.turnRight(90);
+//				} else {
+//					motors.turnLeft(90);
+//					Delay.msDelay(500);
+//					motors.driveForward(Toolbox.BOT_WIDTH/2); //FIXME: adjust this distance according to the width of the device
+//					Delay.msDelay(500);
+//					motors.turnLeft(90);
+//				}
+//				collideCounter++;
+//				Delay.msDelay(500);
+//				motors.driveForward(); //we are facing the right way, let's continue the search by
+//						       		   //driving forward
+//			}
+//			
+//			if (colorChecker.getCurrentColor() == 0) {
+//				motors.stopDriveMotors();
+//				LCD.drawString("Found the treasure!", 1, 1);
+//				Delay.msDelay(3000);
+//				foundTreasure = true;
+//				
+//			}
+//		}
+//	}
+//}
